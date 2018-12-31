9b1e29486c5e54cd420781964338b40db4bc5af7
HendrikB <hendri.db@gmail.com>
2018-12-31 20:39:00 +0000

    Added new diagonal press features
    Added diagonal press features. Also added capacitive surface touch checks.

--- oculus go and gear vr/GetVRButtonDown.cs
+++ oculus go and gear vr/GetVRButtonDown.cs
@@ -16,7 +16,6 @@ namespace HutongGames.PlayMaker.Actions
 
     public class GetVRButtonDown : FsmStateAction
     {
-
         // Events to send
         // Trigger button pressed
         [ActionSection("Trigger Button")]
@@ -83,6 +82,58 @@ namespace HutongGames.PlayMaker.Actions
         [Tooltip("Touchpad right press sensitivity. How far should the button be pressed to the right of the touchpad before it triggers as being pressed right? Defaults to 0.5, which means about 50% right from centre. This feels just right, but you can set it less or more depending on your needs.")]
         public FsmFloat rightSensitivity = 0.5f;
 
+        // Touchpad Diagonal Pressed Direction
+        [ActionSection("Touchpad Pressed Diagonally")]
+        [Tooltip("Checks if Touchpad is pressed in the upper-left direction.")]
+        public FsmEvent touchpadPressedUpperLeft;
+        [Tooltip("Sensitivity for X. Ranges from 0 to -1. It represents the distance from the centre of the touchpad to the left. -0.3 should work ok, but you can set this for less or more sensitivity.")]
+        public FsmFloat upperLeftSensitivityX = -0.3f;
+        [Tooltip("Sensitivity for Y. Ranges from 0 to 1. It represents the distance from the centre of the touchpad to the top. 0.3 should work ok, but you can set this for less or more sensitivity.")]
+        public FsmFloat upperLeftSensitivityY = 0.3f;
+
+        [Tooltip("Checks if Touchpad is pressed in the upper-right direction.")]
+        public FsmEvent touchpadPressedUpperRight;
+        [Tooltip("Sensitivity for X. Ranges from 0 to 1. It represents the distance from the centre of the touchpad to the right. 0.3 should work ok, but you can set this for less or more sensitivity.")]
+        public FsmFloat upperRightSensitivityX = 0.3f;
+        [Tooltip("Sensitivity for Y. Ranges from 0 to 1. It represents the distance from the centre of the touchpad to the top. 0.3 should work ok, but you can set this for less or more sensitivity.")]
+        public FsmFloat upperRightSensitivityY = 0.3f;
+
+        [Tooltip("Checks if Touchpad is pressed in the lower-right direction.")]
+        public FsmEvent touchpadPressedLowerRight;
+        [Tooltip("Sensitivity for X. Ranges from 0 to 1. It represents the distance from the centre of the touchpad to the right. 0.3 should work ok, but you can set this for less or more sensitivity.")]
+        public FsmFloat lowerRightSensitivityX = 0.3f;
+        [Tooltip("Sensitivity for Y. Ranges from 0 to -1. It represents the distance from the centre of the touchpad to the bottom. -0.3 should work ok, but you can set this for less or more sensitivity.")]
+        public FsmFloat lowerRightSensitivityY = -0.3f;
+
+        [Tooltip("Checks if Touchpad is pressed in the lower-left direction.")]
+        public FsmEvent touchpadPressedLowerLeft;
+        [Tooltip("Sensitivity for X. Ranges from 0 to -1. It represents the distance from the centre of the touchpad to the left. -0.3 should work ok, but you can set this for less or more sensitivity.")]
+        public FsmFloat lowerLeftSensitivityX = -0.3f;
+        [Tooltip("Sensitivity for Y. Ranges from 0 to -1. It represents the distance from the centre of the touchpad to the bottom. -0.3 should work ok, but you can set this for less or more sensitivity.")]
+        public FsmFloat lowerLeftSensitivityY = -0.3f;
+
+        // Capacitive Surface Touched
+        [ActionSection("Capacitive Surface")]
+        [Tooltip("Checks if capacitive surface of touchpad is being touched towards the top.")]
+        public FsmEvent capacitiveTouchedUp;
+        [Tooltip("Capacitive surface upward press sensitivity. How far should the surface be touched to the top of the capacitive surface before it triggers as being pressed up? Defaults to 0.5, which means about 50% up from centre. This feels just right, but you can set it less or more depending on your needs.")]
+        public FsmFloat capacitiveUpSensitivity = 0.5f;
+
+        [Tooltip("Checks if capacitive surface of touchpad is being touched towards the bottom.")]
+        public FsmEvent capacitiveTouchedDown;
+        [Tooltip("Capacitive surface downward press sensitivity. How far should the surface be touched to the bottom of the capacitive surface before it triggers as being pressed down? Defaults to 0.5, which means about 50% down from centre. This feels just right, but you can set it less or more depending on your needs.")]
+        public FsmFloat capacitiveDownSensitivity = -0.5f;
+
+        [Tooltip("Checks if capacitive surface of touchpad is being touched towards the left.")]
+        public FsmEvent capacitiveTouchedLeft;
+        [Tooltip("Capacitive surface left press sensitivity. How far should the surface be touched to the left of the capacitive surface before it triggers as being pressed left? Defaults to 0.5, which means about 50% left from centre. This feels just right, but you can set it less or more depending on your needs.")]
+        public FsmFloat capacitiveLeftSensitivity = -0.5f;
+
+        [Tooltip("Checks if capacitive surface of touchpad is being touched towards the right.")]
+        public FsmEvent capacitiveTouchedRight;
+        [Tooltip("Capacitive surface right press sensitivity. How far should the surface be touched to the right of the capacitive surface before it triggers as being pressed right? Defaults to 0.5, which means about 50% right from centre. This feels just right, but you can set it less or more depending on your needs.")]
+        public FsmFloat capacitiveRightSensitivity = 0.5f;
+
         // Resets all values to defaults
         public override void Reset()
         {
@@ -97,10 +148,41 @@ namespace HutongGames.PlayMaker.Actions
             touchpadSwipeDown = null;
             touchpadSwipeLeft = null;
             touchpadSwipeRight = null;
+            touchpadPressedUp = null;
+            touchpadPressedDown = null;
+            touchpadPressedLeft = null;
+            touchpadPressedRight = null;
+            upSensitivity = 0.5f;
+            downSensitivity = -0.5f;
+            leftSensitivity = -0.5f;
+            rightSensitivity = 0.5f;
+            touchpadPressedUpperLeft = null;
+            touchpadPressedUpperRight = null;
+            touchpadPressedLowerRight = null;
+            touchpadPressedLowerLeft = null;
+            upperLeftSensitivityX = -0.3f;
+            upperLeftSensitivityY = 0.3f;
+            upperRightSensitivityX = 0.3f;
+            upperRightSensitivityY = 0.3f;
+            lowerRightSensitivityX = 0.3f;
+            lowerRightSensitivityY = -0.3f;
+            lowerLeftSensitivityX = -0.3f;
+            lowerLeftSensitivityY = -0.3f;
+            capacitiveTouchedUp = null;
+            capacitiveTouchedDown = null;
+            capacitiveTouchedLeft = null;
+            capacitiveTouchedRight = null;
+            capacitiveUpSensitivity = 0.5f;
+            capacitiveDownSensitivity = -0.5f;
+            capacitiveLeftSensitivity = -0.5f;
+            capacitiveRightSensitivity = 0.5f;
         }
 
         public override void OnUpdate()
         {
+            // Touchpad axis
+            touchpadAxis.Value = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+
             // Trigger button pressed
             if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
             {
@@ -153,8 +235,27 @@ namespace HutongGames.PlayMaker.Actions
                 Fsm.Event(touchpadSwipeRight);
             }
 
-            // Touchpad axis
-            touchpadAxis.Value = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+            // Touchpad pressed in a diagonal direction
+            // Upper-right
+            if ((touchpadAxis.Value.x >= upperRightSensitivityX.Value) && (touchpadAxis.Value.y >= upperRightSensitivityY.Value) && (OVRInput.GetDown(OVRInput.Button.One)))
+            {
+                Fsm.Event(touchpadPressedUpperRight);
+            }
+            // Lower-right
+            if ((touchpadAxis.Value.x >= lowerRightSensitivityX.Value) && (touchpadAxis.Value.y <= lowerRightSensitivityY.Value) && (OVRInput.GetDown(OVRInput.Button.One)))
+            {
+                Fsm.Event(touchpadPressedLowerRight);
+            }
+            // Lower-left
+            if ((touchpadAxis.Value.x <= lowerLeftSensitivityX.Value) && (touchpadAxis.Value.y <= lowerLeftSensitivityY.Value) && (OVRInput.GetDown(OVRInput.Button.One)))
+            {
+                Fsm.Event(touchpadPressedLowerLeft);
+            }
+            // Upper-left
+            if ((touchpadAxis.Value.x <= upperLeftSensitivityX.Value) && (touchpadAxis.Value.y >= upperLeftSensitivityY.Value) && (OVRInput.GetDown(OVRInput.Button.One)))
+            {
+                Fsm.Event(touchpadPressedUpperLeft);
+            }
 
             // Touchpad pressed in a specific direction
             if ((touchpadAxis.Value.x > rightSensitivity.Value) && OVRInput.GetDown(OVRInput.Button.One))
@@ -174,27 +275,23 @@ namespace HutongGames.PlayMaker.Actions
                 Fsm.Event(touchpadPressedDown);
             }
 
-        }
-
-        public override string ErrorCheck()
-        {
-            if (FsmEvent.IsNullOrEmpty(triggerButtonPressed) &&
-                FsmEvent.IsNullOrEmpty(triggerButtonReleased) &&
-                FsmEvent.IsNullOrEmpty(triggerButtonHeld) &&
-                FsmEvent.IsNullOrEmpty(backButton) &&
-                FsmEvent.IsNullOrEmpty(touchpadButtonPressed) &&
-                FsmEvent.IsNullOrEmpty(touchpadButtonReleased) &&
-                FsmEvent.IsNullOrEmpty(touchpadButtonHeld) &&
-                FsmEvent.IsNullOrEmpty(touchpadPressedUp) &&
-                FsmEvent.IsNullOrEmpty(touchpadPressedDown) &&
-                FsmEvent.IsNullOrEmpty(touchpadPressedLeft) &&
-                FsmEvent.IsNullOrEmpty(touchpadPressedRight) &&
-                FsmEvent.IsNullOrEmpty(touchpadSwipeUp) &&
-                FsmEvent.IsNullOrEmpty(touchpadSwipeDown) &&
-                FsmEvent.IsNullOrEmpty(touchpadSwipeLeft) &&
-                FsmEvent.IsNullOrEmpty(touchpadSwipeRight))
-                return "Action sends no events!";
-            return "";
+            // Capacitive surface touched in a specific direction
+            if (touchpadAxis.Value.x > rightSensitivity.Value)
+            {
+                Fsm.Event(capacitiveTouchedRight);
+            }
+            if (touchpadAxis.Value.x < leftSensitivity.Value)
+            {
+                Fsm.Event(capacitiveTouchedLeft);
+            }
+            if (touchpadAxis.Value.y > upSensitivity.Value)
+            {
+                Fsm.Event(capacitiveTouchedUp);
+            }
+            if (touchpadAxis.Value.y < downSensitivity.Value)
+            {
+                Fsm.Event(capacitiveTouchedDown);
+            }
         }
     }
 }
